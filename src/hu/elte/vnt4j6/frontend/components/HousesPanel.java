a0e2e5aff1542dffbc047d0c05f4489977395ddc
lcsoka <laszlo@eclick.co.hu>
2018-11-14 17:24:43 +0100

    (feat) houses panel gui

--- src/hu/elte/vnt4j6/frontend/components/HousesPanel.java
+++ src/hu/elte/vnt4j6/frontend/components/HousesPanel.java
@@ -6,19 +6,212 @@
 package hu.elte.vnt4j6.frontend.components;
 
 import hu.elte.vnt4j6.frontend.windows.MainWindow;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
  *
  * @author lcsoka
  */
-public class HousesPanel extends JPanel {
-    
+public final class HousesPanel extends JPanel {
+
     private final MainWindow window;
-    
-  public HousesPanel(MainWindow frame) {
+    private GridBagLayout gbLayout;
+    private GridBagConstraints gbc;
+    private JPanel headerPanel;
+    private JPanel contentPanel;
+    private JPanel footerPanel;
+    private JLabel titleLbl;
+    private JLabel houseNameLbl;
+    private JLabel studentCountLbl;
+    private JButton addBtn;
+    private JButton editBtn;
+
+    public HousesPanel(MainWindow frame) {
         this.window = frame;
-//        initDashBoardPanel();
-//        initButtons();
-    }   
+        initHousePanels();
+        initButtons();
+    }
+
+    public void initHousePanels() {
+        gbLayout = new GridBagLayout();
+        gbc = new GridBagConstraints();
+        setLayout(gbLayout);
+
+        initHeaderPanel();
+        initContentPanel();
+        initFooterPanel();
+    }
+    
+    private void initHeaderPanel() {
+        headerPanel = new JPanel();
+        GridBagLayout gbHeaderPanel = new GridBagLayout();
+        GridBagConstraints gbcHeaderPanel = new GridBagConstraints();
+        headerPanel.setLayout(gbHeaderPanel);
+
+        addBtn = new JButton("Add");
+        gbcHeaderPanel.gridx = 18;
+        gbcHeaderPanel.gridy = 1;
+        gbcHeaderPanel.gridwidth = 1;
+        gbcHeaderPanel.gridheight = 1;
+        gbcHeaderPanel.fill = GridBagConstraints.HORIZONTAL;
+        gbcHeaderPanel.weightx = 1;
+        gbcHeaderPanel.weighty = 0;
+        gbcHeaderPanel.anchor = GridBagConstraints.EAST;
+        gbcHeaderPanel.insets = new Insets(10, 10, 10, 10);
+        gbHeaderPanel.setConstraints(addBtn, gbcHeaderPanel);
+        headerPanel.add(addBtn);
+
+        titleLbl = new JLabel("House");
+        gbcHeaderPanel.gridx = 1;
+        gbcHeaderPanel.gridy = 1;
+        gbcHeaderPanel.gridwidth = 8;
+        gbcHeaderPanel.gridheight = 1;
+        gbcHeaderPanel.fill = GridBagConstraints.BOTH;
+        gbcHeaderPanel.weightx = 1;
+        gbcHeaderPanel.weighty = 1;
+        gbcHeaderPanel.anchor = GridBagConstraints.WEST;
+        gbcHeaderPanel.insets = new Insets(10, 10, 10, 10);
+        gbHeaderPanel.setConstraints(titleLbl, gbcHeaderPanel);
+        headerPanel.add(titleLbl);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 20;
+        gbc.gridheight = 3;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.weightx = 1;
+        gbc.weighty = 0;
+        gbc.anchor = GridBagConstraints.NORTH;
+        gbLayout.setConstraints(headerPanel, gbc);
+        add(headerPanel);
+    }
+    
+    private void initContentPanel(){
+        contentPanel = new JPanel();
+        contentPanel.setBorder(BorderFactory.createTitledBorder(""));
+        GridBagLayout gbContentPanel = new GridBagLayout();
+        GridBagConstraints gbcContentPanel = new GridBagConstraints();
+        contentPanel.setLayout(gbContentPanel);
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.gridwidth = 20;
+        gbc.gridheight = 14;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.weightx = 0;
+        gbc.weighty = 1;
+        gbc.anchor = GridBagConstraints.NORTH;
+        gbLayout.setConstraints(contentPanel, gbc);
+        add(contentPanel);
+
+    }
+    
+    private void initFooterPanel() {
+        footerPanel = new JPanel();
+        GridBagLayout gbFooterPanel = new GridBagLayout();
+        GridBagConstraints gbcFooterPanel = new GridBagConstraints();
+        footerPanel.setLayout(gbFooterPanel);
+
+        JLabel houseNameLbl = new JLabel("House name:");
+        gbcFooterPanel.gridx = 0;
+        gbcFooterPanel.gridy = 1;
+        gbcFooterPanel.gridwidth = 1;
+        gbcFooterPanel.gridheight = 1;
+        gbcFooterPanel.fill = GridBagConstraints.HORIZONTAL;
+        gbcFooterPanel.weightx = 0;
+        gbcFooterPanel.weighty = 0;
+        gbcFooterPanel.anchor = GridBagConstraints.WEST;
+        gbcFooterPanel.insets = new Insets(10, 10, 0, 10);
+        gbFooterPanel.setConstraints(houseNameLbl, gbcFooterPanel);
+        footerPanel.add(houseNameLbl);
+
+        JLabel studentCountLbl = new JLabel("Student count:");
+        gbcFooterPanel.gridx = 0;
+        gbcFooterPanel.gridy = 2;
+        gbcFooterPanel.gridwidth = 1;
+        gbcFooterPanel.gridheight = 1;
+        gbcFooterPanel.fill = GridBagConstraints.HORIZONTAL;
+        gbcFooterPanel.weightx = 0;
+        gbcFooterPanel.weighty = 0;
+        gbcFooterPanel.anchor = GridBagConstraints.WEST;
+        gbcFooterPanel.insets = new Insets(10, 10, 10, 10);
+        gbFooterPanel.setConstraints(studentCountLbl, gbcFooterPanel);
+        footerPanel.add(studentCountLbl);
+
+        editBtn = new JButton("Edit");
+        gbcFooterPanel.gridx = 18;
+        gbcFooterPanel.gridy = 2;
+        gbcFooterPanel.gridwidth = 1;
+        gbcFooterPanel.gridheight = 1;
+        gbcFooterPanel.fill = GridBagConstraints.HORIZONTAL;
+        gbcFooterPanel.weightx = 1;
+        gbcFooterPanel.weighty = 0;
+        gbcFooterPanel.anchor = GridBagConstraints.EAST;
+        gbcFooterPanel.insets = new Insets(10, 10, 10, 10);
+        gbFooterPanel.setConstraints(editBtn, gbcFooterPanel);
+        footerPanel.add(editBtn);
+
+        houseNameLbl = new JLabel("Test");
+        gbcFooterPanel.gridx = 1;
+        gbcFooterPanel.gridy = 1;
+        gbcFooterPanel.gridwidth = 1;
+        gbcFooterPanel.gridheight = 1;
+        gbcFooterPanel.fill = GridBagConstraints.BOTH;
+        gbcFooterPanel.weightx = 0;
+        gbcFooterPanel.weighty = 0;
+        gbcFooterPanel.anchor = GridBagConstraints.WEST;
+        gbcFooterPanel.insets = new Insets(10, 0, 0, 0);
+        gbFooterPanel.setConstraints(houseNameLbl, gbcFooterPanel);
+        footerPanel.add(houseNameLbl);
+
+        studentCountLbl = new JLabel("35");
+        gbcFooterPanel.gridx = 1;
+        gbcFooterPanel.gridy = 2;
+        gbcFooterPanel.gridwidth = 1;
+        gbcFooterPanel.gridheight = 1;
+        gbcFooterPanel.fill = GridBagConstraints.BOTH;
+        gbcFooterPanel.weightx = 0;
+        gbcFooterPanel.weighty = 0;
+        gbcFooterPanel.anchor = GridBagConstraints.WEST;
+        gbcFooterPanel.insets = new Insets(10, 0, 10, 0);
+        gbFooterPanel.setConstraints(studentCountLbl, gbcFooterPanel);
+        footerPanel.add(studentCountLbl);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 17;
+        gbc.gridwidth = 20;
+        gbc.gridheight = 3;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.weightx = 0;
+        gbc.weighty = 1;
+        gbc.anchor = GridBagConstraints.SOUTH;
+        gbLayout.setConstraints(footerPanel, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 17;
+        gbc.gridwidth = 20;
+        gbc.gridheight = 3;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        gbc.anchor = GridBagConstraints.SOUTH;
+        gbLayout.setConstraints(footerPanel, gbc);
+        add(footerPanel);
+    }
+
+    private void initButtons() {
+        addBtn.addActionListener(this::addHouse);
+    }
+    
+    private void addHouse(ActionEvent e) {
+        window.addHouse();
+    }
 }
--- src/hu/elte/vnt4j6/frontend/windows/MainWindow.java
+++ src/hu/elte/vnt4j6/frontend/windows/MainWindow.java
@@ -6,38 +6,71 @@
 package hu.elte.vnt4j6.frontend.windows;
 
 import hu.elte.vnt4j6.frontend.components.HousesPanel;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.HeadlessException;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 /**
  *
  * @author lcsoka
  */
 public class MainWindow extends JFrame {
+
     private static JPanel actionPanel;
+    private static JTabbedPane tabs;
+    private Dimension size = new Dimension(600, 450);
 //    private static HousesPanel contentPanel;
-    
-    public MainWindow()throws HeadlessException {
+
+    public MainWindow() throws HeadlessException {
         initScreen();
     }
-    
-     private void initScreen() {
+
+    private void initScreen() {
         setLayout(new FlowLayout());
 //        contentPanel = new HousesPanel(this);
         actionPanel = new JPanel();
+        tabs = new JTabbedPane();
+       
+        tabs.addTab("Houses", new HousesPanel(this));
+        tabs.setSelectedIndex(0);
+        JPanel jplInnerPanel2 = createInnerPanel("Tab 2 Contains Icon only");
+        tabs.addTab("Students", jplInnerPanel2);
+        JPanel jplInnerPanel3 = createInnerPanel("Tab 3 Contains Tooltip and Icon");
+        tabs.addTab("Creatures", jplInnerPanel3);
+        JPanel jplInnerPanel4 = createInnerPanel("Tab 4 Contains Text only");
+        tabs.addTab("Personalities", jplInnerPanel4);
+        // Add the tabbed pane to this panel.
+        actionPanel.setLayout(new GridLayout(1, 1));
+        add(tabs);
+
+        actionPanel.add(tabs);
+
         add(actionPanel);
-        setSize(500, 500);
+        setSize(size);
+        setPreferredSize(size);
+        setMinimumSize(size);
         setTitle("Beadando");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        System.out.println(getSize().width);
-        System.out.println(getSize().height);
         this.getContentPane().setLayout(new GridLayout(1, 1));
+    }
 
-        
+    protected JPanel createInnerPanel(String text) {
+        JPanel jplPanel = new JPanel();
+        JLabel jlbDisplay = new JLabel(text);
+        jlbDisplay.setHorizontalAlignment(JLabel.CENTER);
+        jplPanel.setLayout(new GridLayout(1, 1));
+        jplPanel.add(jlbDisplay);
+        return jplPanel;
     }
     
-    
+    public void addHouse(){
+        System.out.println("Opening add house window...");
+    }
+
 }
